6cc5f6fcc860cc394bf7613b324f63b4a1435432
Pramod Mane <pramod.mane703@gmail.com>
2025-08-19 11:28:43 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,4 @@
+first_name = input("Enter the First Name: ")
+last_name = input("Enter the Second Name: ")
+
+print(f"Hello, {first_name} {last_name}! Welcome to the Python program.")
---
+++ mathematics.py
@@ -0,0 +1,7 @@
+num_1  = int(input("Enter the First Number: "))
+num_2  = int(input("Enter the Second Number: "))
+
+print("Addition:",num_1 + num_2)
+print("Subraction:",num_1 - num_2)
+print("Multiplication:",num_1 * num_2)
+print("Division:",num_1 / num_2)
